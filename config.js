b1fd3aee8db2d3f1b2e0db5afe1fbad7ab61fb2a
Rafał Lipieta <cwgflojdid@wp.pl>
2018-08-02 19:05:18 +0200

    Google login app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '384456212233-kbuhrb2ta1tprch6bt4qsa1nqancui1l.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'lC-4t0ZMuFDyQD3b-TTNFSM5',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
